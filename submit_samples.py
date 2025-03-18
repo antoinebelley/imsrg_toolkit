14fc35616009c7470cd469368a6bac5a21f0794e
antoinebelley <antoine.belley@me.com>
2025-03-18 12:46:04 -0400

    Fixed bug to run multiple operators

--- submit_samples.py
+++ submit_samples.py
@@ -13,9 +13,9 @@ import pandas as pd
 
 
 ##########PARAMETERS TO CHANGE BEFORE RUN###################
-emax = [4,6,8]
-time = ["00:10:00", "00:15:00", "02:00:00"]
-memory = ['1G', "4G", "20G"]
+emax = [4,6,8, 10]
+time = ["00:10:00", "00:15:00", "02:00:00", "08:00:00"]
+memory = ['1G', "4G", "20G", "100G"]
 # mass =  [17]
 # emax = [10]
 # time = ["08:00:00"]
@@ -36,16 +36,12 @@ def getNucl(Nucl, A):
 
 
 LECs = ['Ct1S0pp','Ct1S0np','Ct1S0nn','Ct3S1','C1S0','C3P0','C1P1','C3P1','C3S1','CE1','C3P2','c1','c2','c3','c4','cD','cE']
-# df = pd.read_csv("/work/submit/abelley/imsrg_toolkit/data/8000Samples.txt")
+df = pd.read_csv("/work/submit/abelley/imsrg_toolkit/data/8000Samples.txt")
 
-# index = np.array(df.index)
-# rng = np.random.default_rng(seed=42)
-# index = rng.choice(index, num_samples, replace=False, shuffle=False)
-c2s = [-1.05,-1,-0.95,-0.9, -0.85, -0.8,-0.75,-0.7,-0.65,-0.6,-0.55,-0.5,-0.45,-0.4,-0.3,-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05 ]
-# c2s = [-0.75,-0.7,-0.65,-0.6,-0.55,-0.5,-0.45,-0.4,-0.3,-0.25]
-weights = [-0.33240194, -0.33954467, -0.33478173, -0.23955455,  2.55206001, 0.45721983,
-   0.02596174, -1.00661369,  0.76901682,  0.3655842,  -0.83388486, -0.75019334,
-  -0.37186834, -0.81926898,  1.04056735,  0.89864506,  0.28259456]
+index = np.array(df.index)
+rng = np.random.default_rng(seed=42)
+index = rng.choice(index, num_samples, replace=False, shuffle=False)
+
 
 
 for A in mass:
@@ -89,15 +85,10 @@ for A in mass:
 #SBATCH --error=/work/submit/abelley/results/kshell_log/errors/test_err_%j.txt
 #SBATCH --time=10:00 """
 
-# count = 0
-    # for i in index:
-    #   sample = df.iloc[i]
-    #   SampleID = int(sample["SampleID"])
-    #   weights = list(sample[LECs])
-    for i,c2 in enumerate(c2s):
-      # sample = df.iloc[i]
-      SampleID = 1e10+i
-      weights[-5] = c2 
+    for i in index:
+      sample = df.iloc[i]
+      SampleID = int(sample["SampleID"])
+      weights = list(sample[LECs])
 
       imsrg_params['header'] = f"""#!/bin/bash
 #SBATCH --job-name={imsrg_params['ref']}_emax{imsrg_params['emax']}_Sample{SampleID}_%j
@@ -120,4 +111,3 @@ export OMP_NUM_THREADS=24
 
       imsrg_submit = Utils(Nucl, [state, state], imsrg_params, kshell_params, SampleID=SampleID)
       imsrg_submit.submit_all_combine_delta(weights, SampleID, f"{imsrg_submit.output_dir}/{imsrg_submit.filebase}_R2p.csv", header_expvals = header_expvals)
-      # count +=1 
